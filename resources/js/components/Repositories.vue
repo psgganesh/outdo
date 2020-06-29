55492400f1faec28ab23397bf20d2f0b70fd0333
Shankar <psgganesh@gmail.com>
2020-06-30 00:42:23 +0800

    pushing repositories

--- resources/js/components/Repositories.vue
+++ resources/js/components/Repositories.vue
@@ -7,28 +7,31 @@
     </va-column>
   </va-row>
   <va-row v-else>
-    <va-column :xs="12" :sm="6" :md="5" :lg="5">
+    <va-column :xs="12" :sm="12" :md="12" :lg="12">
       <va-table :hover="hover" :size="size">
         <table>
           <thead>
             <tr>
-              <th>Repository</th>
-              <th>Author</th>
+              <th colspan="2">
+                Repository
+              </th>
+              <th colspan="2">
+                Author
+              </th>
+              <th>Default branch</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="(repository, index) in repositories" :key="index">
-              <td><avatar :src="repository.links.avatar.href" /> {{ repository.full_name }}</td>
+              <td><avatar :src="repository.links.avatar.href" :size="28" /></td>
+              <td>{{ repository.full_name }}</td>
+              <td><avatar :src="repository.owner.links.avatar.href" :size="28" /></td>
               <td>{{ repository.owner.display_name }}</td>
+              <td><va-icon color="primary" padding="5px" type="code-branch" />{{ repository.mainbranch.name }}</td>
             </tr>
           </tbody>
         </table>
       </va-table>
-    </va-column>
-    <va-column :xs="12" :sm="6" :md="7" :lg="7">
-      <div>
-        xs: 12, sm: 6, md: 8
-      </div>
     </va-column>
   </va-row>
 </template>
